f7f71e683b88b189a1f54d1d46d14dd8cbaaa157
Mauko Maunde <maukoese@gmail.com>
2019-04-13 11:30:44 +0300

    Update mpesa-cf7.php

--- mpesa-cf7.php
+++ mpesa-cf7.php
@@ -4,5 +4,16 @@ echo '<script>
 document.addEventListener( "wpcf7mailsent", function( event ) {
     location = "http://example.com/";
 }, false );
+
+document.addEventListener( "wpcf7submit", function( event ) {
+    var inputs = event.detail.inputs;
+ 
+    for ( var i = 0; i < inputs.length; i++ ) {
+        if ( 'your-name' == inputs[i].name ) {
+            alert( inputs[i].value );
+            break;
+        }
+    }
+}, false );
 </script>';
 });
